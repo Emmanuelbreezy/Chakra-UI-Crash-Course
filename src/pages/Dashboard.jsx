28089ed4120204eb603d71f8dfd3d3aa467a967b
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2024-03-02 18:29:19 +0100

    lesson 2

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,38 @@
+import { Heading, Container, Box, Text } from "@chakra-ui/react";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const boxStyle = {
+    p: "10px",
+    bg: "blue.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    ":hover": {
+      color: "#333",
+      bg: "blue.300",
+    },
+  };
+  return (
+    <Container as="main" maxW="5xl" py="15px">
+      <Heading my="30px" p="10px">
+        React Chakra
+      </Heading>
+
+      <Text marginLeft="20px">We are learning chakraui</Text>
+
+      <Text ml="20px" color="green.500" fontWeight="500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Text>
+
+      <Box sx={boxStyle}>Subscribe to TechWithEmma</Box>
+
+      <Box marginY="10px" p="20px" bg="green.500">
+        <Text color="white">
+          Lorem dolor sit, amet consectetur adipisicing elit
+        </Text>
+      </Box>
+    </Container>
+  );
 };
 
 export default Dashboard;
